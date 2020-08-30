19e3af22becb235caddfd5536c6cc004ac67f200
Udayraj Mandke <urmandke@gmail.com>
2020-08-30 12:00:24 +0530

    A mess, will clean up when I understand what is going on

---
+++ CmakeTutorial/build/CMakeFiles/toDo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toDo.dir/ToDo.cc.o"
+  "CMakeFiles/toDo.dir/main.cc.o"
+  "toDo"
+  "toDo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toDo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
