576d1ebe3a299bb4de458f8e7abad8c06b0a8d07
Udayraj Mandke <urmandke@gmail.com>
2020-09-02 15:08:53 +0530

    Constants

---
+++ Youtube/Constants.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <float.h>
+#define X 10
+
+using std::cout;
+using std::cin;
+
+
+int main()
+{
+    const int x = 5;
+    cout << x << "\n" << std::endl;
+    cout << X << "\n" << std::endl;
+    enum { y = 100 };
+    cout << y << "\n" << std::endl;
+
+    return 0;
+}
+
