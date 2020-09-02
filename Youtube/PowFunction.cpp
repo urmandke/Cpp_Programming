723df6b70d6ad8c8fef94c7f61b3c9a247e2916c
Udayraj Mandke <urmandke@gmail.com>
2020-09-02 14:05:02 +0530

    Custom power function

--- Youtube/PowFunction.cpp
+++ Youtube/PowFunction.cpp
@@ -4,13 +4,25 @@
 using std::cout;
 using std::cin;
 
+double power(double base, double  exponent)
+{
+    double result = 1;
+    for (int i = 0;i < exponent;i++)
+    {
+        result = result * base;
+    }
+    return result;
+}
+
 int main()
 {
     int base, exponent;
 
     cin >> base;
     cin >> exponent;
-    cout << pow(base,exponent) << "\n";
+    double myPower = power(base,exponent);
+    cout << myPower << std::endl;
     
     return 0;
 }
+
