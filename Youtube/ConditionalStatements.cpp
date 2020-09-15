bfd427897af676a758d020b309fb9a6a823ab944
Udayraj Mandke <urmandke@gmail.com>
2020-09-15 15:13:39 +0530

    if/else example

---
+++ Youtube/ConditionalStatements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int age = 11;
+    if(age < 13)
+    {
+        std::cout << "You're not old enough\n";
+    }
+    else
+    {
+        std::cout << "False\n";
+    }
+    
+
+    std::cout << "Always";
+    return 0;
+}
